b1b4e2f557d6cbcb72125a99cd4e0de9b566290a
Jaana Ahola <jaanaaholaa@gmail.com>
2024-02-20 12:47:51 +0200

    muokkauksia toimivammaksi

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+    exports.getData = functions.https.onRequest((req, res) => {
+        const db = admin.firestore();
+        // Fetch data from Firestore
+        db.collection('data').get()
+          .then(snapshot => {
+            const data = [];
+            snapshot.forEach(doc => {
+              data.push(doc.data());
+            });
+            return res.status(200).json(data);
+          })
+          .catch(error => {
+            console.error('Error getting documents', error);
+            return res.status(500).send('Error getting data');
+          });
+      });
